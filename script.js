a9021317d2be07c057f8f33c1e89b90ca39cde81
0i000000000a7 <140218829+Final0i00000000a7@users.noreply.github.com>
2023-12-09 20:50:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ function loop(ms) {
   if (typeof game.leastBoost=="undefined") game.leastBoost=Infinity
   game.collapseTime += ms/1000
   game.base = 9
-  game.ord = new ExpantaNum(4.45e88)
+  game.ord = new ExpantaNum(4.12e160)
   if (game.pupgrades.includes(11)) game.base = 80
   if (game.pupgrades.includes(12)) game.base = 72
   if (game.pupgrades.includes(15)) game.base = 64
